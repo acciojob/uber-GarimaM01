f4c8248c018058d946351cf652663ff21d17fc92
GarimaM01 <garima.mahto@sakec.ac.in>
2023-03-18 16:21:15 +0530

    Uber-Assignment-Attempt2

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -6,33 +6,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.driver.model.Admin;
+import com.driver.model.Driver;
+import com.driver.model.Customer;
+import java.util.*;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-
+@Autowired AdminService adminService;
 	@PostMapping("/register")
-	public ResponseEntity<Void> registerAdmin(@RequestBody Admin admin){
-		return new ResponseEntity<>(HttpStatus.OK);
+	public ResponseEntity<Void> registerAdmin(@RequestBody Admin admin){ adminService.adminRegister(admin); return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@PutMapping("/update")
 	public ResponseEntity<Admin> updateAdminPassword(@RequestParam Integer adminId, @RequestParam String password){
-		return new ResponseEntity<>(updatedAdmin, HttpStatus.OK);
+		return new ResponseEntity<>(adminService.updatePassword(adminId,password), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete")
-	public void deleteAdmin(@RequestParam Integer adminId){
+	public void deleteAdmin(@RequestParam Integer adminId){ adminService.deleteAdmin(adminId);
 	}
 
 	@GetMapping("/listOfCustomers")
 	public List<Customer> listOfCustomers() {
-		return listOfCustomers;
+		return adminService.getListOfCustomers();
 	}
 
 	@GetMapping("/listOfDrivers")
 	public List<Driver> listOfDrivers() {
-		return listOfDrivers;
+		return adminService.getListOfDrivers();
 	}
 }
